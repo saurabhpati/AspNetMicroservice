5950ce852f636827f2e694c70811adad5e98b7ad
Saurabh Pati <Saurabh.Pati@grapecity.com>
2023-12-25 21:31:40 +0530

    microservices start.

---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _catalogContext;
+
+        public ProductRepository(ICatalogContext catalogContext)
+        {
+            _catalogContext = catalogContext ?? throw new ArgumentNullException(nameof(catalogContext));
+        }
+
+        public Task CreateProduct(Product product)
+        {
+            return _catalogContext.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            var result = await _catalogContext.Products.DeleteOneAsync(id);
+            return result.IsAcknowledged && result.DeletedCount > 0;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            var result = await _catalogContext.Products.Find<Product>(p => true).ToListAsync();
+            return result;
+        }
+
+        public Task<Product> GetProducts(string id)
+        {
+            return _catalogContext.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByName(string name)
+        {
+            var result = await _catalogContext.Products
+                .Find(Builders<Product>.Filter.Eq(p => p.Name, name))
+                .ToListAsync();
+
+            return result;
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var result = await _catalogContext.Products.ReplaceOneAsync(filter: p => p.Id == product.Id, replacement: product);
+
+            return result.IsAcknowledged && result.IsModifiedCountAvailable;
+        }
+    }
+}
